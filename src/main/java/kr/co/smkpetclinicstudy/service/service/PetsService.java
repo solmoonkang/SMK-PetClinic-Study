1dbb377d841cc410d6b62b8101e749e3d278d58c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-03-21 16:49:31 +0900

    Feat : Create Owners & Pets & Vets Service Class

---
+++ src/main/java/kr/co/smkpetclinicstudy/service/service/PetsService.java
@@ -0,0 +1,9 @@
+package kr.co.smkpetclinicstudy.service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PetsService {
+}
