e2d1cbdc9ef6a43d16d8cf20b80a1bd12bee08b1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-03-21 16:18:17 +0900

    init

---
+++ src/main/java/kr/co/smkpetclinicstudy/SmkPetClinicStudyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.smkpetclinicstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmkPetClinicStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmkPetClinicStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/smkpetclinicstudy/SmkPetClinicStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.smkpetclinicstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmkPetClinicStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
