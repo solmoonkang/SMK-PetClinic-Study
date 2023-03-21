99dbbefca197632502f159da1554837ddb772a56
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-03-21 16:48:31 +0900

    Feat : Create Owners & Pets & Vets Controller Class

---
+++ src/main/java/kr/co/smkpetclinicstudy/controller/VetsController.java
@@ -0,0 +1,11 @@
+package kr.co.smkpetclinicstudy.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vets")
+@RequiredArgsConstructor
+public class VetsController {
+}
