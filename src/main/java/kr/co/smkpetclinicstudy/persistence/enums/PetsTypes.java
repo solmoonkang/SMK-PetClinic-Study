ac6c4b89fd0a59e0e706d5ea0509044e1b850c17
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-04-04 23:06:37 +0900

    Fix : Fix PetsTypes

--- src/main/java/kr/co/smkpetclinicstudy/persistence/enums/PetsTypes.java
+++ src/main/java/kr/co/smkpetclinicstudy/persistence/enums/PetsTypes.java
@@ -1,20 +1,31 @@
 package kr.co.smkpetclinicstudy.persistence.enums;
 
 import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
+@AllArgsConstructor
 public enum PetsTypes {
 
-    MALTESE("maltese"),
-    SHIHTZU("shihtzu"),
-    BICHON("bichon"),
-    POODLE("poodle");
+    DOG("강아지"),
 
-    @JsonValue      // 직렬화
-    private final String value;
+    CAT("고양이"),
 
-    PetsTypes(String value) {
-        this.value = value;
+    BIRD("새"),
+
+    REPTILE("파충류"),
+
+    OTHER("기타");
+
+    String petType;
+
+    public static PetsTypes of(String petType){
+
+        return Arrays.stream(PetsTypes.values())
+                .filter(type -> type.toString().equalsIgnoreCase(petType))
+                .findAny().orElseThrow(() -> new RuntimeException("Not Fount Pet Type"));
     }
 }
