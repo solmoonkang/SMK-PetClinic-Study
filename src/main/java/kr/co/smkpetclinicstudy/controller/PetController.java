9437dc41c619c8ae39a29d80b81df40130602260
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-04-09 03:21:43 +0900

    Feat : Implementation Pet Exception & ResponseFormat

--- src/main/java/kr/co/smkpetclinicstudy/controller/PetController.java
+++ src/main/java/kr/co/smkpetclinicstudy/controller/PetController.java
@@ -1,11 +1,13 @@
 package kr.co.smkpetclinicstudy.controller;
 
-import jakarta.validation.Valid;
+import kr.co.smkpetclinicstudy.infra.global.error.enums.ErrorCode;
+import kr.co.smkpetclinicstudy.infra.global.error.response.ResponseFormat;
+import kr.co.smkpetclinicstudy.infra.global.exception.NotFoundException;
 import kr.co.smkpetclinicstudy.service.model.dtos.request.PetReqDTO;
 import kr.co.smkpetclinicstudy.service.model.dtos.response.PetResDTO;
 import kr.co.smkpetclinicstudy.service.service.PetService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,41 +20,49 @@ public class PetController {
     private final PetService petService;
 
     @PostMapping
-    public ResponseEntity<String> createPet(@RequestBody @Valid PetReqDTO.CREATE create) {
+    public ResponseFormat<String> createPet(@RequestBody @Validated PetReqDTO.CREATE create) {
         try {
             petService.createPet(create);
-            return ResponseEntity.ok("Successfully Create Pet");
-        } catch (Exception e) {
-            return ResponseEntity.ok("Error : " + e);
+            return ResponseFormat.successMessage(
+                    ErrorCode.SUCCESS_CREATED,
+                    "펫 정보가 성공적으로 생성되었습니다");
+        } catch (NotFoundException e) {
+            return ResponseFormat.fail(ErrorCode.FAIL);
         }
     }
 
     @GetMapping("/{owner_id}")
-    public ResponseEntity<List<PetResDTO.READ>> getPetsByOwnerId(@PathVariable(name = "pet_id") Long ownerId) throws Exception {
+    public ResponseFormat<List<PetResDTO.READ>> getPetsByOwnerId(@PathVariable(name = "owner_id") Long ownerId) {
         try {
-            return ResponseEntity.ok(petService.getPetsByOwnerId(ownerId));
-        } catch (Exception e) {
-            throw new Exception("Error : " + e);
+            return ResponseFormat.successData(
+                    ErrorCode.SUCCESS_EXECUTE,
+                    petService.getPetsByOwnerId(ownerId));
+        } catch (NotFoundException e) {
+            return ResponseFormat.fail(ErrorCode.FAIL);
         }
     }
 
     @PutMapping
-    public ResponseEntity<String> updatePet(@RequestBody @Valid PetReqDTO.UPDATE update) {
+    public ResponseFormat<String> updatePet(@RequestBody @Validated PetReqDTO.UPDATE update) {
         try {
             petService.updatePet(update);
-            return ResponseEntity.ok("Successfully Update Pet");
-        } catch (Exception e) {
-            return ResponseEntity.ok("Error : " + e);
+            return ResponseFormat.successMessage(
+                    ErrorCode.SUCCESS_EXECUTE,
+                    "펫 정보가 성공적으로 수정되었습니다");
+        } catch (NotFoundException e) {
+            return ResponseFormat.fail(ErrorCode.FAIL);
         }
     }
 
     @DeleteMapping("/{pet_id}")
-    public ResponseEntity<String> deletePet(@PathVariable(name = "pet_id") Long petId) {
+    public ResponseFormat<String> deletePet(@PathVariable(name = "pet_id") Long petId) {
         try {
             petService.deletePetById(petId);
-            return ResponseEntity.ok("Successfully Create Pet");
-        } catch (Exception e) {
-            return ResponseEntity.ok("Error : " + e);
+            return ResponseFormat.successMessage(
+                    ErrorCode.SUCCESS_EXECUTE,
+                    "펫 정보가 성공적으로 삭제되었습니다");
+        } catch (NotFoundException e) {
+            return ResponseFormat.fail(ErrorCode.FAIL);
         }
     }
 }
