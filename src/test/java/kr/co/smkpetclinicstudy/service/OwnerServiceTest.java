770ff409f39e6f005babd4a0e9d90430f5f5ce1c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-04-11 21:48:00 +0900

    Feat : Implementation OwnerServiceTest createOwner

---
+++ src/test/java/kr/co/smkpetclinicstudy/service/OwnerServiceTest.java
@@ -0,0 +1,129 @@
+package kr.co.smkpetclinicstudy.service;
+
+import kr.co.smkpetclinicstudy.infra.global.exception.DuplicatedException;
+import kr.co.smkpetclinicstudy.persistence.entity.Owner;
+import kr.co.smkpetclinicstudy.persistence.repository.OwnerRepository;
+import kr.co.smkpetclinicstudy.service.model.dtos.request.OwnerReqDTO;
+import kr.co.smkpetclinicstudy.service.model.dtos.response.OwnerResDTO;
+import kr.co.smkpetclinicstudy.service.model.mappers.OwnerMapper;
+import kr.co.smkpetclinicstudy.service.service.OwnerService;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class OwnerServiceTest {
+
+    // Spring 컨테이너가 Service 객체를 생성하고 의존성 주입을 자동으로 처리한다
+    // 따라서 Service 객체를 직접 생성할 필요 없이, Test Class에 @Autowired 을 사용하여 Service 객체를 주입받을 수 있다
+    // 이 방식은 TestCode 작성 시 유연성을 높여주며, 코드 중복을 줄일 수 있어 코드의 가독성을 높여준다
+    @InjectMocks
+    private OwnerService ownerService;
+
+    @Mock
+    private OwnerRepository ownerRepository;
+
+    @Mock
+    private OwnerMapper ownerMapper;
+
+    private OwnerReqDTO.CREATE create;
+
+    private OwnerReqDTO.UPDATE update;
+
+    private Owner owner;
+
+    @BeforeEach
+    void setUpOwnerCreate() {
+        // OwnerReqDTO.CREATE 객체 초기화 코드
+        // Owner owner 객체 초기화 코드
+        create = OwnerReqDTO.CREATE.builder()
+                .firstName("SOLMOON")
+                .lastName("KANG")
+                .address("BOO-YOUNG APT 301, Inchang-Dong")
+                .city("GURI-SI")
+                .telephone("010-3884-2913")
+                .build();
+
+        owner = new Owner(
+                create.getFirstName(),
+                create.getLastName(),
+                create.getAddress(),
+                create.getCity(),
+                create.getTelephone());
+    }
+
+    @AfterEach
+    void tearDownOwnerCreate() {
+    }
+
+    @Test
+    @DisplayName("createOwner Service Test")
+    public void createOwnerTest() {
+        // given
+        given(ownerMapper.ownerCreateDtoToEntity(eq(create))).willReturn(owner);
+
+        // when
+        ownerService.createOwner(create);
+
+        // then
+        verify(ownerRepository, times(1)).save(eq(owner));
+    }
+
+    @Test
+    @DisplayName("createOwner Service Test with Duplicate Owner Telephone")
+    public void createOwnerTest_withDuplicateOwnerTelephone() {
+        // given : Owner 의 전화번호를 이미 존재하는 상황을 가정한다
+        // willReturn : void 메소드에서 호출 후 예외를 던질 때 사용된다
+        given(ownerRepository.existsByTelephone(create.getTelephone())).willReturn(true);
+
+        // when : createOwner 메소드가 DuplicatedException 을 던지는지 확인한다
+        assertThrows(DuplicatedException.class, () -> ownerService.createOwner(create));
+
+        // then : Owner 가 저장되지 않았음을 검증한다
+        verify(ownerRepository, never()).save(owner);
+    }
+
+
+    @Test
+    @DisplayName("getOwnerById Service Test")
+    public void getOwnerByIdTest() {
+        Long ownerId = 1L;
+        Owner owner = Owner.builder()
+                .firstName("SOLMOON")
+                .lastName("KANG")
+                .address("BOO-YOUNG APT 301, Inchang-Dong")
+                .city("GURI-SI")
+                .telephone("010-3884-2913")
+                .build();
+
+        OwnerResDTO.READ expected = OwnerResDTO.READ.builder()
+                .ownerId(ownerId)
+                .firstName("SOLMOON")
+                .lastName("KANG")
+                .city("GURI-SI")
+                .build();
+
+        // given
+            // thenReturn : 스텁 메소드의 기본 반환 값을 설정하는데 사용된다(스텁 메소드 : 호출된 경우 지정된 값을 반환한다)
+        when(ownerRepository.findById(ownerId)).thenReturn(Optional.of(owner));
+
+        // when
+        OwnerResDTO.READ actual = ownerService.getOwnerById(ownerId);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+        verify(ownerRepository, times(1)).findById(ownerId);
+        // 더 이상 Mock 객체와 상호작용이 없는지 검증한다
+        verifyNoInteractions(ownerRepository);
+    }
+}
