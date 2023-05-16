fd1d7636990f342e2ea16d0c052eec898c092dbe
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-05-16 20:39:54 +0900

    Feat : Add created/modifiedAt BaseEntity & JpaAuditing

--- src/main/java/kr/co/smkpetclinicstudy/persistence/BaseEntity.java
+++ src/main/java/kr/co/smkpetclinicstudy/persistence/BaseEntity.java
@@ -3,12 +3,26 @@ package kr.co.smkpetclinicstudy.persistence;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
 
 @MappedSuperclass
 @Getter
 @EqualsAndHashCode(callSuper = false)      // equals & hashCode 메서드를 자동 생성
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    protected LocalDate createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at")
+    protected LocalDate modifiedAt;
 }
