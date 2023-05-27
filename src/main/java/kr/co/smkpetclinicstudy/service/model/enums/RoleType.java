25fa2fbe3ec86ed438a341473a0e8028067df428
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-05-27 19:45:16 +0900

    Feat : Add Admin Entity Class & RoleType Enum Class

---
+++ src/main/java/kr/co/smkpetclinicstudy/service/model/enums/RoleType.java
@@ -0,0 +1,13 @@
+package kr.co.smkpetclinicstudy.service.model.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum RoleType {
+
+    USER_ROLE,
+
+    ADMIN_ROLE
+}
