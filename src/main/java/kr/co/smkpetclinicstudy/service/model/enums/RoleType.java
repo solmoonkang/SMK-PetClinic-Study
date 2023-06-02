53528e4c9b6f9df4fe2e6bf554b30fbcc56caf90
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-06-02 18:43:32 +0900

    Feat : Add RoleType Name

--- src/main/java/kr/co/smkpetclinicstudy/service/model/enums/RoleType.java
+++ src/main/java/kr/co/smkpetclinicstudy/service/model/enums/RoleType.java
@@ -1,13 +1,26 @@
 package kr.co.smkpetclinicstudy.service.model.enums;
 
+import kr.co.smkpetclinicstudy.infra.global.error.enums.ErrorCode;
+import kr.co.smkpetclinicstudy.infra.global.exception.NotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.Arrays;
 
 @Getter
 @AllArgsConstructor
 public enum RoleType {
 
-    USER_ROLE,
+    ROLE_USER("사용자"),
 
-    ADMIN_ROLE
+    ROLE_ADMIN("관리자");
+
+
+    String roleType;
+
+    public static RoleType of(String roleType){
+        return Arrays.stream(RoleType.values())
+                .filter(type -> type.toString().equalsIgnoreCase(roleType))
+                .findAny().orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_MEMBER));
+    }
 }
